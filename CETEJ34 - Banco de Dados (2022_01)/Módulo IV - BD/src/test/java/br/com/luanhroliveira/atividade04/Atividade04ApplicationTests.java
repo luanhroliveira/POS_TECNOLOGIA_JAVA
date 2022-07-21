d32a32351259e40b873d666e59d46a4d80203083
luanhroliveira <luanhigor98@gmail.com>
2022-07-21 20:26:20 -0300

    [CETEJ34 - Banco de Dados (2022_01)] - atividade 04

---
+++ CETEJ34 - Banco de Dados (2022_01)/Módulo IV - BD/src/test/java/br/com/luanhroliveira/atividade04/Atividade04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luanhroliveira.atividade04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
